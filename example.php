9b2ba99e81116a8d16f59ded6d26fe75fd2255ae
Tayfun Erbilen <tayfunerbilen@gmail.com>
2014-10-08 17:47:57 +0300

    Create example.php

---
+++ example.php
@@ -0,0 +1,20 @@
+<?php
+
+require 'HTML_Encrypt.php';
+
+$html = '<!doctype html>
+<html>
+<head>
+  <meta charset="utf8">
+  <title>Example</title>
+</head>
+<body>
+
+  <h1>Welcome Test Page</h1>
+
+</body>
+</html>';
+
+echo HTML_Encrypt::encrypt($html);
+
+?>
